62114ec91c63b74e151128ff8a589271bbdd176b
Aeon234 <32624715+Aeon234@users.noreply.github.com>
2025-01-21 18:07:33 -0800

    Update page.tsx

--- src/app/rosterData/page.tsx
+++ src/app/rosterData/page.tsx
@@ -4,7 +4,7 @@ function RosterData() {
   return (
     <div className="RosterApp animated animatedFadeInUp fadeInUp h-[900px] max-h-[1000px]">
       <h1 className="text-2xl text-white font-bold text-center m-2">
-        Roster Application Data Results
+        Roster Data
       </h1>
       <div className="flex justify-between items-start space-x-4 text-center">
         <div className="bg-slate-800 text-white p-10 rounded shadow-lg flex-1">
