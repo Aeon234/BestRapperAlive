135e52951a69174a4fbff2b52389cc6300732578
Aeon234 <32624715+Aeon234@users.noreply.github.com>
2025-06-25 13:47:36 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,10 @@ import bg from "../../public/bg-leather.jpg";
 export default function Home() {
   return (
     // <div className="h-screen w-full">
-    <main className={`h-screen w-full grid place-items-center bg-[${bg}]`}>
+    <main
+      className="h-screen w-full grid place-items-center b"
+      style={{ backgroundImage: `url(${bg.src})` }}
+    >
       <Form />
     </main>
     // </div>
