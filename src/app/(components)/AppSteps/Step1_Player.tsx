470a9c2b31155f1e4ee9f54d80fdcd9725f17780
Aeon234 <32624715+Aeon234@users.noreply.github.com>
2025-06-25 13:04:03 -0700

    Update Step1_Player.tsx

--- src/app/(components)/AppSteps/Step1_Player.tsx
+++ src/app/(components)/AppSteps/Step1_Player.tsx
@@ -4,7 +4,7 @@ import { TextCustom } from "../TextCustom";
 function Player() {
   return (
     <div className="w-full">
-      <TextCustom name="playerName" label="Name" placeholder="e.g. Aeon" />
+      <TextCustom name="playerName" label="Names" placeholder="e.g. Aeon" />
     </div>
   );
 }
