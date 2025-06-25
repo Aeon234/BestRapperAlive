98a27a22946f735d8b0fe592a2eca4453296f4a1
Aeon234 <32624715+Aeon234@users.noreply.github.com>
2025-06-25 13:44:36 -0700

    Fix image path references and import background image
    Updated image URLs in Form.tsx to use absolute paths for header.jpg. In page.tsx, imported bg-leather.jpg and used it as a dynamic background image in the main element.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Form from "./Application/Form";
 
+import bg from "../../public/bg-leather.jpg";
+
 export default function Home() {
   return (
     // <div className="h-screen w-full">
-    <main className="h-screen w-full grid place-items-center bg-[url(bg-leather.jpg)]">
+    <main className={`h-screen w-full grid place-items-center bg-[${bg}]`}>
       <Form />
     </main>
     // </div>
